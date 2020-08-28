42a48a1a0d48c2906de9da8ca0d343afe3ab6b07
Glauber Modolo Cabral <glauber.sp@gmail.com>
2020-08-28 15:19:21 -0300

    Create app with app format.

---
+++ android/app/src/main/kotlin/com/glauber/app1/app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.glauber.app1.app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/firstPage.dart
@@ -0,0 +1,61 @@
+library app1;
+
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  static const routeName = '/first';
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<dynamic, dynamic> args =
+        ModalRoute.of(context).settings.arguments ?? {};
+    final String title =
+        args.containsKey('title') ? args['title'] : "First Page";
+    final message = args.containsKey('message')
+        ? args['message']
+        : "Message from First Page";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FirstPageBody(message: message),
+    );
+  }
+}
+
+class FirstPageBody extends StatelessWidget {
+  const FirstPageBody({
+    Key key,
+    @required this.message,
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("Hello, $message!"),
+          RaisedButton(
+            child: Text("Go to Second"),
+            onPressed: () {
+              Navigator.pushNamed(context, '/second',
+                      arguments: {'message': message})
+                  .then((value) => Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text(value as String),
+                      )));
+            },
+          ),
+          RaisedButton(
+            child: Text("Back"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
